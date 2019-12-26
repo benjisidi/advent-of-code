9f497106fdeeb28d9f1d9cc6bc3e533bd8c32f9c
Benji Sidi <benji.sidi@gmail.com>
2019-12-26 20:30:18 +0000

    Convert day 1 to python3 and pep8

--- day1/1-1.py
+++ day1/1-1.py
@@ -1,7 +1,10 @@
 import math
+
+
 def getFuel(mass):
-	return math.floor(mass/3) - 2
+    return math.floor(mass/3) - 2
+
 
 with open("./1-1.txt", "r") as f:
-	fuels = [getFuel(int(x)) for x in f.readlines()]
-	print sum(fuels)
+    fuels = [getFuel(int(x)) for x in f.readlines()]
+    print(sum(fuels))
--- day1/1-2.py
+++ day1/1-2.py
@@ -1,13 +1,15 @@
 import math
 
+
 def getFuel(mass, totalFuel=0):
-	requiredFuel = math.floor(mass/3) - 2
-	if requiredFuel <= 0:
-		return totalFuel
-	else:
-		return getFuel(requiredFuel, totalFuel + requiredFuel)
-	return math.floor(mass/3) - 2
+    requiredFuel = math.floor(mass/3) - 2
+    if requiredFuel <= 0:
+        return totalFuel
+    else:
+        return getFuel(requiredFuel, totalFuel + requiredFuel)
+    return math.floor(mass/3) - 2
+
 
 with open("./1-1.txt", "r") as f:
-	fuels = [getFuel(int(x)) for x in f.readlines()]
-	print sum(fuels)
+    fuels = [getFuel(int(x)) for x in f.readlines()]
+    print(sum(fuels))
